a89ed86d292734db6fca03738e96a09636c20b3c
Deimon3332 <Deimon1mail@gmail.com>
2022-05-27 15:27:01 +0300

    Добавьте файлы проекта.

---
+++ Emoji/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+
+
+/*Эмоджи и символы можно вводить с помощью последовательности \x , \u  и тд, а так же можно выводить отдельные символы
+ * например \f. 
+ * Везде используется кодировка Unicode.
+ * Для ввывода большего разнообразия эмоджи нужны определенные расширения .
+ */
+
+
+namespace emoji
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            
+            Console.WriteLine("x263B"+   "\f" );
+        }
+    }
+}
